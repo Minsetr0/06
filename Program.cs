5b31a83ce1b48101eef3c1fdf4a0a78b0552ac35
Minsetr0 <nesterovmaksim71@mail.ru>
2023-02-21 19:19:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,120 +2,118 @@
 const string CommandChangePassword = "SetPassword";
 const string CommandChangeColor = "ChangeCharColor";
 const string CommandCalculator = "Calculator";
-const int Eagle = 0;
-const int Tails = 1;
-const int MinCoinNumber = 0;
-const int MaxCoinNumber = 2;
-const string colorRed = "Red";
-const string colorGreen = "Green";
-const string colorWhite = "White";
-const string commandExit = "yes";
-const string sum = "+";
-const string subtraction = "-";
-const string divide = "/";
-const string multiply = "*";
-const string moduleDivide = "%";
+const string CommandExit = "yes";
 
-Random rand = new Random();
-string choiceInCoin, password, choiseColor, inputExit;
-int coinSide;
-float conclusion, number1, number2;
-string action;
+string inputExit = "";
 
-Console.WriteLine("Желаете войти в программу? Введите yes, если да.");
-inputExit = Console.ReadLine();
-
-if (inputExit == "yes")
+Console.WriteLine("Добро пожаловать в моё приложение.");
+while (CommandExit != inputExit)
 {
-    Console.WriteLine("Добро пожаловать в моё приложение.");
-    while (commandExit == inputExit)
+    Console.WriteLine($"Что вы хотите сделать?\n\n{CommandCoin} - игра в монетку\n{CommandChangePassword} - установить пароль" +
+    $"\n{CommandChangeColor} - изменить цвет всех символов\n{CommandCalculator} - начать работу в калькуляторе(Пока доступно действие только с двумя числами).");
+    string choice = Console.ReadLine();
+
+    switch (choice)
     {
-        Console.WriteLine($"Что вы хотите сделать?\n\n{CommandCoin} - игра в монетку\n{CommandChangePassword} - установить пароль" +
-        $"\n{CommandChangeColor} - изменить цвет всех символов\n{CommandCalculator} - начать работу в калькуляторе(Пока доступно действие только с двумя числами).");
-        string choice = Console.ReadLine();
+        case CommandCoin:
 
-        switch (choice)
-        {
-            case CommandCoin:
-                Console.WriteLine("Орел или решка?");
-                choiceInCoin = Console.ReadLine();
-                coinSide = rand.Next(MinCoinNumber, MaxCoinNumber);
+            int eagle = 0;
+            int tails = 1;
+            int minCoinNumber = 0;
+            int maxCoinNumber = 2;
 
-                if (coinSide == Eagle)
-                {
-                    Console.WriteLine("Вам выпал Орел.");
-                }
-                else if (coinSide == Tails)
-                {
-                    Console.WriteLine("Вам выпала Решка.");
-                }
+            Random rand = new Random();
+            int coinSide;
 
-                break;
+            Console.WriteLine("Орел или решка?");
+            string choiceInCoin = Console.ReadLine();
+            coinSide = rand.Next(minCoinNumber, maxCoinNumber);
 
-            case CommandChangePassword:
-                Console.WriteLine("Введите свой пароль.");
-                password = Console.ReadLine();
-                break;
+            if (coinSide == eagle)
+            {
+                Console.WriteLine("Вам выпал Орел.");
+            }
+            else if (coinSide == tails)
+            {
+                Console.WriteLine("Вам выпала Решка.");
+            }
 
-            case CommandChangeColor:
-                Console.WriteLine("Пока что доступно только 3 цвета: зеленый(введите Green), красный(введите Red), а так же классический белый(введите White)");
-                choiseColor = Console.ReadLine();
-                if (choiseColor == colorGreen)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                }
-                else if (choiseColor == colorWhite)
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-                else if (choiseColor == colorRed)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                }
+            break;
 
-                break;
-            case CommandCalculator:
-                Console.WriteLine("Введите 1 число:");
-                number1 = Console.Read();
-                Console.WriteLine("Введите действие:");
-                action = Console.ReadLine();
-                Console.WriteLine("Введите 3 число:");
-                number2 = Console.Read();
-                switch (action)
-                {
-                    case sum:
-                        conclusion = number1 + number2;
-                        Console.WriteLine(conclusion); 
-                        break;
-                    case subtraction:
-                        conclusion = number1 - number2;
-                        Console.WriteLine(conclusion);
-                        break;
-                    case divide:
-                        conclusion = number1 / number2;
-                        Console.WriteLine(conclusion);
-                        break;
-                    case multiply:
-                        conclusion = number1 * number2;
-                        Console.WriteLine(conclusion);
-                        break;
-                    case moduleDivide:
-                        conclusion = number1 * number2;
-                        Console.WriteLine(conclusion);
-                        break;
-                }
+        case CommandChangePassword:
+            Console.WriteLine("Введите свой пароль.");
+            string password = Console.ReadLine();
+            break;
 
-                break;
-                default:
-                Console.WriteLine("Такой команды не существует!!!");
-                break;
-        }
-        Console.Clear();
-        Console.WriteLine("Напишите yes для повторного входа в приложение,");
-        inputExit = Console.ReadLine();
+        case CommandChangeColor:
+
+            string colorRed = "Red";
+            string colorGreen = "Green";
+            string colorWhite = "White";
+
+            Console.WriteLine($"Пока что доступно только 3 цвета: зеленый(введите {colorGreen}), красный(введите {colorRed}), а так же классический белый(введите {colorWhite})");
+            string choiseColor = Console.ReadLine();
+            if (choiseColor == colorGreen)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (choiseColor == colorWhite)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (choiseColor == colorRed)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            break;
+        case CommandCalculator:
+
+            const string Sum = "+";
+            const string Subtraction = "-";
+            const string Divide = "/";
+            const string Multiply = "*";
+            const string ModuleDivide = "%";
+
+            float number1;
+            float number2;
+            string action;
+            float conclusion;
+
+            Console.WriteLine("Введите 1 число:");
+            number1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите действие:");
+            action = Console.ReadLine();
+            Console.WriteLine("Введите 3 число:");
+            number2 = Convert.ToSingle(Console.ReadLine());
+            switch (action)
+            {
+                case Sum:
+                    conclusion = number1 + number2;
+                    Console.WriteLine($"Ваш ответ: {conclusion}");
+                    break;
+                case Subtraction:
+                    conclusion = number1 - number2;
+                    Console.WriteLine($"Ваш ответ: {conclusion}");
+                    break;
+                case Divide:
+                    conclusion = number1 / number2;
+                    Console.WriteLine($"Ваш ответ: {conclusion}");
+                    break;
+                case Multiply:
+                    conclusion = number1 * number2;
+                    Console.WriteLine($"Ваш ответ: {conclusion}");
+                    break;
+                case ModuleDivide:
+                    conclusion = number1 * number2;
+                    Console.WriteLine($"Ваш ответ: {conclusion}");
+                    break;
+            }
+            Console.ReadLine();
+
+            break;
     }
-}
-else if(inputExit != "yes")
-{
-    Console.WriteLine("Заходите в следующий раз!");
+    Console.Clear();
+    Console.WriteLine($"Напишите {CommandExit} для выхода из приложения, для продолжения работы напишите что угодно.");
+    inputExit = Console.ReadLine();
 }
